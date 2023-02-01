50693be795b2397aecad20619845a9a717c02d8e
ReeHam001 <reeham-d@hotmail.com>
2023-02-01 01:53:02 -0800

    ADD IndexController for fontend - Function : index

---
+++ app/Http/Controllers/Frontend/IndexController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers\Frontend;
+
+use App\Http\Controllers\Controller;
+use App\Models\Post;
+
+use Illuminate\Http\Request;
+
+
+class IndexController extends Controller
+{
+    public function index()
+    {
+        $posts = Post::with(['media', 'user'])
+            ->whereHas('category', function ($query) {
+                $query->whereStatus(1);
+            })
+            ->whereHas('user', function ($query) {
+                $query->whereStatus(1);
+            })
+         ->orderBy('id', 'desc')->paginate(5);
+
+        return view('frontend.index', compact('posts'));
+    }
+
+}
